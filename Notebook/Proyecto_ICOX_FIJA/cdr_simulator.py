9a1b8760aafd3f1af2d51389647f2eedfeacb794
Aitor Elordi Zamora <124396119+aitorez@users.noreply.github.com>
2023-10-20 22:16:29 +0200

    mas

--- Notebook/Proyecto_ICOX_FIJA/cdr_simulator.py
+++ Notebook/Proyecto_ICOX_FIJA/cdr_simulator.py
@@ -1,10 +1,16 @@
 import random
 import pandas as pd
 from datetime import datetime, timedelta
+from functions import*
 
 # Genera un número de teléfono MOC con prefijo español y 9 dígitos adicionales
-def generar_numero_moc():
+def generar_numero_moc_origen():
     
+    prefijo_espanol = "+34"  # Prefijo español
+    nueve_digitos = "".join([str(random.randint(0, 9)) for _ in range(9)])
+    return f"{prefijo_espanol}{nueve_digitos}"
+
+def generar_numero_moc_destino():
     prefijo_espanol = "+34"  # Prefijo español
     
     probabilidad_numeros_especiales = 0.25  # 25% de probabilidad de número especial
@@ -14,11 +20,21 @@ def generar_numero_moc():
     else:
         nueve_digitos = "".join([str(random.randint(0, 9)) for _ in range(9)])
         return f"{prefijo_espanol}{nueve_digitos}"
-
-
+    
 # Genera un número de teléfono aleatorio para MTC
 def generar_numero_mtc():
-    return f"+{random.randint(1, 99)}{random.randint(100, 999)}{random.randint(1000, 9999)}"
+    prefijo_espanol = "+34"
+    probabilidad_numeros_especiales = 0.25  # 25% de probabilidad de número especial
+    if random.random() < probabilidad_numeros_especiales:
+        numeros_especiales = ["1002","1003","1004","100", "123", "911", "0800", "555", "666", "777", "888", "999", "112", "113", "114", "115", "116", "117", "118", "119", "900", "901", "902", "903", "904", "905", "906", "907", "908", "909"]
+        return random.choice(numeros_especiales)
+    
+    else:
+        nueve_digitos = "".join([str(random.randint(0, 9)) for _ in range(9)])
+        return f"{prefijo_espanol}{nueve_digitos}"
+    
+    
+    
 
 # Genera una duración de llamada aleatoria en segundos
 def generar_duracion_llamada():
@@ -26,19 +42,25 @@ def generar_duracion_llamada():
 
 # Genera una fecha y hora aleatoria en los últimos 30 días
 def generar_fecha_hora():
-    ahora = datetime.now()
-    fecha_hora = ahora - timedelta(days=random.randint(1, 30))
-    return fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+    fecha_inicio = datetime(2023, 1, 1)
+    fecha_fin = datetime(2023, 12, 31)
+    # Genera una fecha aleatoria dentro del rango de 2023
+    fecha_hora = fecha_inicio + timedelta(days=random.randint(0, (fecha_fin - fecha_inicio).days))
+    # Genera una hora aleatoria en formato HH:MM:SS
+    hora = f"{random.randint(0, 23):02d}:{random.randint(0, 59):02d}:{random.randint(0, 59):02d}"
+    
+    # Combina la fecha y la hora
+    return f"{fecha_hora.strftime('%Y-%m-%d')} {hora}"
 
 # Crea un registro de CDR realista
 def crear_registro_cdr():
     if random.random() < 0.6:
-        origen = generar_numero_moc()
+        origen = generar_numero_moc_origen()
         destino = generar_numero_mtc()
         tipo = "MOC"  # Llamada emitida
     else:
         origen = generar_numero_mtc()
-        destino = generar_numero_moc()
+        destino = generar_numero_moc_origen()
         tipo = "MTC"  # Llamada recibida
     
     duracion = generar_duracion_llamada()
@@ -64,9 +86,10 @@ def generar_dataframe_cdrs(cantidad):
     return pd.DataFrame(cdrs)
 
 # Ejemplo de generación de un DataFrame de CDRs
-cantidad_registros = 5000
+cantidad_registros = 100000
 df_cdrs = generar_dataframe_cdrs(cantidad_registros)
 df_cdrs['Minutos'] = (df_cdrs['Duracion']/60).round(2)
+
 # Limpiar el signo "+" de las columnas "Telefono1" y "Telefono2"
 df_cdrs["Origen_corregido"] = df_cdrs["Origen"].str.replace("+", "")
 df_cdrs["Destino_corregido"] = df_cdrs["Destino"].str.replace("+", "")
---
+++ Notebook/Proyecto_ICOX_FIJA/functions.py
@@ -0,0 +1,1 @@
+def maxima_coincidencia():
